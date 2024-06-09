463990e86eb1f3fd49d4f2ce628dc52a92f3416e
Zara-Khan-7 <zaara.khan1795.zy@gmail.com>
2024-06-10 02:46:32 +0500

    Adding project 01

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+let user2 = {
+    name: "zaara",
+    age: 29,
+};
+console.log(user2);
+let user1 = {
+    name: "zaara",
+    age: 29,
+};
+console.log(user1.name);
+console.log(user1.age);
---
+++ main.ts
@@ -0,0 +1,18 @@
+// Making a variable
+let user2={
+    name:"zaara",
+    age:29,
+};
+console.log(user2);
+
+type user={
+    name:string,
+    age:number,
+    
+}; let user1:user={
+    name:"zaara",
+    age:29,
+};
+console.log(user1.name);
+
+console.log(user1.age);
